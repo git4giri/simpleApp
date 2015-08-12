032903c69dd77ddbc7c4068ba6d148b66feed404
jazz4giri <giri.subrata@gmail.com>
2015-08-12 21:10:15 +0530

    init changes

---
+++ UCDSimpleApp/src/com/ibm/test/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.ibm.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet implements Servlet{
+	private static final long serialVersionUID = -3537062746450602871L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp){
+		doPost(req, resp);
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp){
+		PrintWriter out = null;
+		try {
+			out = resp.getWriter();
+			out.write("Hi There !");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			out.close();
+		}
+	}
+	
+}
